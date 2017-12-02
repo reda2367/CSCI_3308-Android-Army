7f1f4622bdedd4739940a5d06ee1060235f7a08f
James Mahon <james.mahon@lasp.colorado.edu>
2017-12-02 02:12:34 -0700

    First version of Details page.  Design not finalized, but database access functional.

---
+++ AndroidArmy_Details.php
@@ -0,0 +1,70 @@
+<html>
+<div id="main">
+<head>
+  <link rel="stylesheet" href="AndroidArmy_Details.css">
+  <link rel="icon" href="https://i.imgur.com/BxSspbo.png">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+  <title> Update Depot </title>
+
+  <div id="logo">
+    <img src="https://i.imgur.com/BxSspbo.png" width="125" height="125" alt="Logo">
+  </div>
+</head>
+<body>
+<h1>
+      <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="AndroidArmy.html">HOME</a>
+        <a href="AndroidArmy_About.html">ABOUT</a>
+        <a href="AndroidArmy_Phones.html">PHONES</a>
+        <a href="AndroidArmy_Submit.html">SUBMIT</a>
+        <a href="AndroidArmy_Contact.html">CONTACT</a>
+      </div>
+
+      <!-- Use any element to open the sidenav -->
+      <span onclick="openNav()"><a href="#menu">
+      &#9776;
+      </a></span>
+</h1>
+<?php
+
+$connection = @mysqli_connect("127.0.0.1","root","moo","project");
+if(mysqli_connect_errno())
+{ 
+echo "<h4>Failed to connect to MySQL: 
+</h4>".mysqli_connect_error(); 
+} 
+
+$manu = $_GET[manufacturer];
+$make = $_GET[make];
+//echo $manu;
+//echo "<br>";
+//echo $make;
+
+$query = "select * from phone where make='$make' and manufacturer='$manu'";
+$resultset = mysqli_query($connection,$query);
+$row = mysqli_fetch_array($resultset, MYSQLI_NUM);
+echo "<h3>";
+echo "<br>";
+echo "<center> Manufacturer: $row[1] </center>";
+echo "<br>";
+echo "<center> Model: $row[2] </center>";
+echo "<br>";
+echo "<center>Android Version: $row[3] </center>";
+echo "<br>";
+echo "<center> OS Release Date: $row[4] </center>";
+echo "<br>";
+echo "<center> Security Update: $row[5] </center>";
+echo "</h3>";
+
+
+?>
+<footer>
+  <p>Created by Emily Childers, Remy Dahlke, Ashley Kim, James Mahon, Neil Nguyen, Adam Ten Hoeve</p>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script src="AndroidArmy.js"></script>
+</body>
+</script>
+</body>
+</html>

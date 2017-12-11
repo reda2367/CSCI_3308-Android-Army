1ca73f9418bd298348358bc4182413df87abf93c
euki8203 <32109777+euki8203@users.noreply.github.com>
2017-12-11 13:54:49 -0700

    added comments

--- AndroidArmy_Contact_Action.php
+++ AndroidArmy_Contact_Action.php
@@ -1,4 +1,4 @@
-<?php
+<?php //request data from the html file
 $name = $_REQUEST['name'];
 $email = $_REQUEST['email'];
 $manufacturer =  $_REQUEST['manufacturer'];
@@ -10,38 +10,44 @@ $recipient = "euki8203@colorado.edu";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 
-
+//email, comment, and name are filled out, send email
 if (!empty ($_REQUEST['email']) && ($_REQUEST['comment']) && ($_REQUEST['name'])) {
   mail($recipient, $subject, $formcontent, $mailheader);
 echo "Thank you $name, your message has been received.";
 exit;
 }
 
+//if email and comment aren't filled out
 if ( (empty ($_REQUEST['email'])) && (empty ($_REQUEST['comment'])) ) {
 echo "ERROR, you did not fill in the <b>Email</b> and the <b>Comment</b> body.";
 exit;
 }
 
+//if email and name aren't filled out
 elseif ( (empty ($_REQUEST['email'])) && (empty ($_REQUEST['name'])) ) {
 echo "ERROR, you did not fill in the <b>Email</b> and the <b>Name</b> body";
 exit;
 }
 
+//if name and comment aren't filled out
 elseif ( (empty ($_REQUEST['name'])) && (empty ($_REQUEST['comment'])) ) {
 echo "ERROR, you did not fill in the <b>Name</b> and the <b>Comment</b> body.";
 exit;
 }
 
+//if email isn't filled out
 elseif (empty ($_REQUEST['email'])) {
 echo "ERROR, you did not fill in your <b>Email.</b> body";
 exit;
 }
 
+//if comment isn't filled out
 elseif (empty ($_REQUEST['comment'])) {
 echo "ERROR, you did not fill in the <b>Comment</b> body.";
 exit;
 }
 
+//if name isn't filled out
 elseif (empty ($_REQUEST['name'])) {
 echo "ERROR, you did not fill in the <b>Name</b> body.";
 exit;
